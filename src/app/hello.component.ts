61771d5cab6c9625270846a14c6ae9a81d58b7d7
ZXinL-CN <1159328306@qq.com>
2022-07-25 10:25:35 +0800

    feat: add emit .

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { LoggerService } from './app.service';
 
 @Component({
   selector: 'hello',
   template: `
   <h3>Hello {{name}}!</h3>
+  <button (click)="notify.emit()">Notify!!</button>
   `,
   styles: [`h1 { font-family: Lato; }`],
 })
 export class HelloComponent implements OnInit {
   constructor(private logger: LoggerService) {}
   @Input() name: string;
+  @Output() notify = new EventEmitter();
   ngOnInit() {
     this.logger.log('ngOnInit');
   }
